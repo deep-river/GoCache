3ef88841e534cb46de7b6b8a5d8beeac2e14efe2
Bangyu Li <bangyuli@buffalo.edu>
2022-03-02 18:48:08 +0800

    Create lru.go

---
+++ lru/lru.go
@@ -0,0 +1,72 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes int64 // LRU可使用内存上限
+	nbytes   int64 // LRU已使用内存大小
+	ll       *list.List // 双向链表
+	cache map[string]*list.Element // 字典,值为双向链表中对应节点的指针
+	OnEvicted func(key string, value Value)
+}
+
+// 双向链表节点数据类型
+type entry struct {
+	key string
+	value Value
+}
+
+type Value interface {
+	Len() int // 返回值所占用的内存大小
+}
+
+func New(maxBytes int64, onEvicted func(string ,Value)) *Cache {
+	return &Cache {
+		maxBytes: maxBytes,
+		ll: list.New(),
+		cache: make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry) // .(TYPE)表示类型动态转换/查询，只能对接口对象使用
+		c.nbytes += int64(value.Len()) - int64(value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry {key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return 
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll. Len()
+}
