77016144f500a7475509c52a9b9ff6f493246c4e
Bangyu Li <bangyuli@buffalo.edu>
2022-03-10 19:00:15 +0800

    Update gocache.go

--- gocache.go
+++ gocache.go
@@ -1,6 +1,10 @@
 package GoCache
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 type Group struct {
 	name      string
@@ -41,4 +45,42 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 	}
 	groups[name] = g
 	return g
+}
+
+func GetGroup(name string) *Group {
+	// Lock(): only one go routine read/write at a time by acquiring the lock.
+	// RLock(): multiple go routine can read(not write) at a time by acquiring the lock.
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is empty")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GoCache] key hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
